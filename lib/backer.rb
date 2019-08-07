5db7eab01b3af366c3725ffa820520c8b56e0c0d
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-08-07 19:22:11 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,16 @@
 class Backer
-  attr_accessor :name, :backed_projects
+  attr_accessor :name
+  attr_reader :backed_projects
+  
   def initialize(name)
     @name = name
     @backed_projects = []
-  end 
+  end
   
-  def back_project(project)
-    @backed_projects << project 
-  project.add_backer(self) unless project.backers.include?(self)
-  end 
-end 
+  def back_project(title)
+   @backed_projects << title
+   title.backers << self
+  end
+  
+  
+end
--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,15 @@
-class Project 
-  attr_accessor :title, :backers
+class Project
+  attr_accessor :title
+  attr_reader :backers
+  
   def initialize(title)
     @title = title
     @backers = []
-  end 
+  end
   
-  def add_backer(backer)
-    @backers << backer
-  backer.back_project(self) unless backer.backed_projects.include?(self)
-end
+  def add_backer(name)
+    @backers << name
+    name.backed_projects << self
+  end
+  
 end
